0addb209a88f2f58e7eacd0592a777e07c055ef8
mcs13 <1010885032@qq.com>
2016-11-13 21:59:18 +0800

    本地版：编造数据demo

--- src/main/java/com/risk/RiskManagerApplication.java
+++ src/main/java/com/risk/RiskManagerApplication.java
@@ -1,12 +1,23 @@
 package com.risk;
 
+import com.risk.Repository.UserDao;
+import com.risk.bean.UserBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class RiskManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RiskManagerApplication.class, args);
+		ConfigurableApplicationContext context=SpringApplication.run(RiskManagerApplication.class, args);
+
+		UserDao userDao=context.getBean(UserDao.class);
+		UserBean user=new UserBean();
+		user.setAccount("mcs");
+		user.setPassword("123");
+		user.setRole(1);
+		userDao.save(user);
 	}
 }
